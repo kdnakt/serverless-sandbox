e38d189027e7a9b1c1bf069dd05f591dc9dc7d69
kdnakt <a.kid.1985@gmail.com>
2020-05-20 02:20:29 +0900

    add aws-sdk

--- typescript/handler.ts
+++ typescript/handler.ts
@@ -1,8 +1,18 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import { DynamoDB } from 'aws-sdk';
+
+const db = new DynamoDB.DocumentClient();
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  console.log(`${process.env.ROOMS_TABLENAME}`)
+  console.log(`${process.env.ROOMS_TABLENAME}`);
+  await db.put({
+    TableName: process.env.ROOMS_TABLENAME,
+    Item: {
+      RoomId: 'DUMMY',
+      DateTime: new Date().toUTCString()
+    }
+  }).promise();
   return {
     statusCode: 200,
     body: JSON.stringify({
