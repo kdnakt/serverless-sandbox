93d251aca4a8463b7c14b2c3beb39cf5987f0390
kdnakt <a.kid.1985@gmail.com>
2020-05-20 21:38:23 +0900

    fix timezone

--- typescript/ApiHandler.ts
+++ typescript/ApiHandler.ts
@@ -6,18 +6,32 @@ const db = new DynamoDB.DocumentClient();
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
   console.log(`${process.env.ROOMS_TABLENAME}`);
+  let res: any = {
+    statusCode: 500
+  };
   await db.put({
     TableName: process.env.ROOMS_TABLENAME,
     Item: {
       RoomId: 'DUMMY',
-      DateTime: new Date().toUTCString()
+      DateTime: new Date().toLocaleString('ja', { timeZone: 'Asia/Tokyo' })
     }
-  }).promise();
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+  }).promise().then(() => {
+    res = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+        input: event,
+      }, null, 2),
+    };
+  }).catch(e => {
+    console.log(e);
+    res = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: e,
+        input: event,
+      }, null, 2),
+    }; 
+  });
+  return res;
 }
