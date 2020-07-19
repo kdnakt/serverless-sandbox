38a07e23135b49d273f7546e50ecfe0986ec8bca
kdnakt <a.kid.1985@gmail.com>
2020-07-19 09:33:42 +0900

    add rss module

--- rss/handler.ts
+++ rss/handler.ts
@@ -1,7 +1,14 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import Parser from 'rss-parser';
+
+const p = new Parser();
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  const targetUrl = process.env.TARGET_URL || 'https://kdnakt.hatenablog.com/feed';
+  console.log('target:', targetUrl);
+  const res = await p.parseURL(targetUrl);
+  console.log('res:', res);
   return {
     statusCode: 200,
     body: JSON.stringify({
