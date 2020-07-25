71433d35e9362b6b654742ab48b321c499e9f2d1
kdnakt <a.kid.1985@gmail.com>
2020-07-25 22:53:57 +0900

    add parse test

---
+++ rss/handler.test.ts
@@ -0,0 +1,32 @@
+import { parse } from './handler';
+import { createServer } from 'http';
+
+const xml = `<!--?xml version="1.0" encoding="UTF-8"?-->
+<feed xml:lang="ja-JP" xmlns="http://www.w3.org/2005/Atom">
+  <id>dummy-blog</id>
+  <entry>
+    <title>Hello World!</title>
+  </entry>
+</feed>`;
+
+test('should parse url', done => {
+  const server = createServer((_, res) => {
+    res.write(xml);
+    res.end();
+  });
+  server.listen(async () => {
+    let url = 'http://localhost';
+    const addr = server.address();
+    if (typeof addr !== 'string') {
+      url += ':' + addr.port;
+    }
+    const output = await parse(url);
+    server.close();
+    expect(output).toEqual({
+      items: [{
+        title: 'Hello World!',
+      }]
+    });
+    done();
+  });
+});
--- rss/handler.ts
+++ rss/handler.ts
@@ -1,13 +1,18 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import Parser from 'rss-parser';
+import * as Parser from 'rss-parser';
 
 const p = new Parser();
+
+export const parse = async (url: string) => {
+  const output = await p.parseURL(url);
+  return output;
+}
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
   const targetUrl = process.env.TARGET_URL || 'https://kdnakt.hatenablog.com/feed';
   console.log('target:', targetUrl);
-  const res = await p.parseURL(targetUrl);
+  const res = await parse(targetUrl);
   console.log('res:', res);
   const isoDate = res.items[0].isoDate;
   console.log('isoDate:', isoDate);
