0d622d96ccd464644a452d91f3c5f168d00a4838
kdnakt <a.kid.1985@gmail.com>
2020-08-22 17:36:40 +0900

    add java-envvar project

---
+++ java-envvar/src/main/java/com/serverless/Handler.java
@@ -0,0 +1,29 @@
+package com.serverless;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private static final Logger LOG = LogManager.getLogger(Handler.class);
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		LOG.info("STAGE: {}", System.getenv("STAGE"));
+		LOG.info("appName: {}", System.getProperty("app.name"));
+		input.put("System::STAGE", System.getenv("STAGE"));
+		input.put("System::appName", System.getProperty("app.name"));
+		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
+	}
+}
