f268494e5f5518f6e5182e8c3affe060fe8be815
kdnakt <a.kid.1985@gmail.com>
2020-08-30 00:59:07 +0900

    add env vars more than 1024

--- java-envvar/src/main/java/com/serverless/Handler.java
+++ java-envvar/src/main/java/com/serverless/Handler.java
@@ -21,6 +21,16 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 		input.put("System::STAGE", System.getenv("STAGE"));
 		input.put("System::appName", System.getProperty("app.name"));
 		input.put("System::appVersion", System.getProperty("app.version"));
+		input.put("mykey1", System.getProperty("mykey1"));
+		input.put("mykey2", System.getProperty("mykey2"));
+		input.put("mykey3", System.getProperty("mykey3"));
+		input.put("mykey4", System.getProperty("mykey4"));
+		input.put("mykey5", System.getProperty("mykey5"));
+		input.put("mykey6", System.getProperty("mykey6"));
+		input.put("mykey7", System.getProperty("mykey7"));
+		input.put("mykey8", System.getProperty("mykey8"));
+		input.put("mykey9", System.getProperty("mykey9"));
+		input.put("java.version", System.getProperty("java.version"));
 		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
